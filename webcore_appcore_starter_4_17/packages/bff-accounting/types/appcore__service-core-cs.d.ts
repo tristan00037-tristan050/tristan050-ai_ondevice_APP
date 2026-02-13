6b0a4cbe6a4836ac59dd39b629d84283f98b0b38
tristan050 <tristan050@naver.com>
2026-02-13 15:35:56 +0900

    Fix: TS7016 add minimal type stub for @appcore/service-core-cs

---
+++ webcore_appcore_starter_4_17/packages/bff-accounting/types/appcore__service-core-cs.d.ts
@@ -0,0 +1,7 @@
+declare module "@appcore/service-core-cs" {
+  export function listTickets(...args: any[]): any;
+  export type CsTicketStatus = any;
+  export const anyExport: any;
+  export = anyExport;
+}
+
