ec6fcaf26a69286b0fc285ceab4ed519e1afa889
tristan00037-tristan050 <tristan00037@gmail.com>
2026-05-30 15:54:48 +0900

    feat(connect-loop): add PR-C Box1 router package init

---
+++ butler_pc_core/connect_loop/__init__.py
@@ -0,0 +1,5 @@
+"""Connect Loop algorithm components."""
+
+from .box1_router import Box1Router, RuleBasedBox1Router, RouterRuntimeContext
+
+__all__ = ["Box1Router", "RuleBasedBox1Router", "RouterRuntimeContext"]
